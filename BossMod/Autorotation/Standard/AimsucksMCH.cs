c487c38db5118ddc89d9cbef16e72036f24f2bc6
Aimsucks <contact@aimsucks.space>
2024-09-09 10:57:12 -0400

    Add basic versions of most oGCD actions

--- BossMod/Autorotation/Standard/AimsucksMCH.cs
+++ BossMod/Autorotation/Standard/AimsucksMCH.cs
@@ -11,6 +11,7 @@ namespace BossMod.Autorotation;
 /*
  * Reference materials:
  * BossMod/BossMod/ActionQueue/ActionDefinition.cs (specifically ChargeCapIn) if we need to figure out if we've used a Drill charge
+ * https://discord.com/channels/1001823907193552978/1191076246860349450/1282433792304222281
  */
 
 public sealed class AimsucksMCH(RotationModuleManager manager, Actor player) : RotationModule(manager, player)
@@ -35,17 +36,18 @@ public sealed class AimsucksMCH(RotationModuleManager manager, Actor player) : R
         None = 0, // All "none" actions are not cast
 
         // Higher priorities will bump actions higher in the queue
-        DelayCombo = 350,
+        DelayedCombo = 350,
         FlexibleCombo = 400,
         ForcedCombo = 880,
 
         // GCD cooldown priorities
         SecondDrill = 590,
         FullMetalField = 600,
-        Excavator = 610,
-        ChainSaw = 620,
-        Drill = 630,
-        AirAnchor = 640,
+        OpenerSecondDrill = 610,
+        Excavator = 620,
+        ChainSaw = 630,
+        Drill = 640,
+        AirAnchor = 650,
 
         // Assign a very high priority for Blazing Shot
         BlazingShot = 900
@@ -56,8 +58,16 @@ public sealed class AimsucksMCH(RotationModuleManager manager, Actor player) : R
         None = 0, // All "none" actions are not cast
 
         // Same priorities for our two main oGCD actions
-        DoubleCheckOrCheckmate = 500
+        DoubleCheckOrCheckmate = 500,
+
+        Reassemble = 600,
+        BarrelStabilizer = 650,
+        Wildfire = 700,
+        Hypercharge = 800
     }
+
+    // Variable to record the combat timer
+    private float CombatTimer;
 
     // Variables to read both of Machinist's gauges, summons, and status
     private int Heat;
@@ -71,13 +81,12 @@ public sealed class AimsucksMCH(RotationModuleManager manager, Actor player) : R
     private MCH.AID NextGCD;
     private GCDPriority NextGCDPrio;
 
-    // Variable for the last used "Check" ability, either Double Check or Checkmate
-    private MCH.AID PrevCheck;
-
     // Variables to get the amount "left" on a buff
     private float FullMetalMachinistLeft;
     private float ExcavatorReadyLeft;
     private float OverheatedLeft;
+    private float ReassembledLeft;
+    private float HyperchargedLeft;
 
     // Variables to get the cooldowns of actions
     private float WildfireCD;
@@ -90,6 +99,29 @@ public sealed class AimsucksMCH(RotationModuleManager manager, Actor player) : R
     private float CD(MCH.AID aid) =>
         World.Client.Cooldowns[ActionDefinitions.Instance.Spell(aid)!.MainCooldownGroup].Remaining;
 
+    // Functions to calculate the time until max charges of an action
+    // and the time until the action's next charge is available
+    private float NextChargeIn(MCH.AID aid) => Unlocked(aid)
+        ? ActionDefinitions.Instance.Spell(aid)!.ReadyIn(World.Client.Cooldowns, World.Client.DutyActions)
+        : float.MaxValue;
+
+    private float MaxChargesIn(MCH.AID aid) => Unlocked(aid)
+        ? ActionDefinitions.Instance.Spell(aid)!.ChargeCapIn(World.Client.Cooldowns, World.Client.DutyActions, Player.Level)
+        : float.MaxValue;
+
+
+    // Functions to check lowest of 4 tool cooldowns for Hypercharge
+    // and the time until the next available tool for Reassemble
+    private float NextToolIn =>
+        MathF.Min(NextChargeIn(MCH.AID.AirAnchor),
+            MathF.Min(NextChargeIn(MCH.AID.Drill),
+                NextChargeIn(MCH.AID.ChainSaw)));
+
+    private float CappedToolIn =>
+        MathF.Min(MaxChargesIn(MCH.AID.AirAnchor),
+            MathF.Min(MaxChargesIn(MCH.AID.Drill),
+                MaxChargesIn(MCH.AID.ChainSaw)));
+
     // Function to check if a GCD can be fit in a certain time window
     private bool CanFitGCD(float deadline, int extraGCDs = 0) => GCD + GCDLength * extraGCDs < deadline;
 
@@ -100,12 +132,17 @@ public sealed class AimsucksMCH(RotationModuleManager manager, Actor player) : R
     private bool IsFirstGCD() => !Player.InCombat || (World.CurrentTime - Manager.CombatStart).TotalSeconds < 0.1f;
 
     /*
-     * Execute is what runs the rotation
-     * We branch a lot of the logic out to individual functions
+     * Execute Rotation
+     * Note: We branch a lot of the logic out to individual functions
+     *
+     * TODO: Queen, Hypercharge, and making sure we don't use Reassemble until buff window in opener
      */
     public override void Execute(StrategyValues strategy, Actor? primaryTarget, float estimatedAnimLockDelay,
         float forceMovementIn, bool isMoving)
     {
+        // Update the combat timer
+        CombatTimer = (float)(World.CurrentTime - Manager.CombatStart).TotalSeconds;
+
         // Update our two gauges
         Heat = GetGauge<MachinistGauge>().Heat;
         Battery = GetGauge<MachinistGauge>().Battery;
@@ -123,40 +160,75 @@ public sealed class AimsucksMCH(RotationModuleManager manager, Actor player) : R
         FullMetalMachinistLeft = SelfStatusLeft(MCH.SID.FullMetalMachinist);
         ExcavatorReadyLeft = SelfStatusLeft(MCH.SID.ExcavatorReady);
         OverheatedLeft = SelfStatusLeft(MCH.SID.Overheated);
+        ReassembledLeft = SelfStatusLeft(MCH.SID.Reassembled);
+        HyperchargedLeft = SelfStatusLeft(MCH.SID.Hypercharged);
 
         // Check our cooldowns and update how long we have left on them
         WildfireCD = CD(MCH.AID.Wildfire);
 
+        // Set our next GCD and next GCD priorities so they can be overwritten by QueueGCD()
+        NextGCD = MCH.AID.None;
+        NextGCDPrio = GCDPriority.None;
+
         // ------------------------
         // Begin processing actions
         // ------------------------
+
+        // oGCD actions
+        UseHypercharge(primaryTarget);
+        UseWildfire(primaryTarget);
+        UseBarrelStabilizer(primaryTarget);
+        UseReassemble(primaryTarget);
+        UseDoubleCheckOrCheckmate(primaryTarget);
+
+        // GCD actions
         UseBlazingShot(primaryTarget);
         UseFullMetalField(primaryTarget);
         UseSingleTargetTools(primaryTarget);
 
-        // Essentially our lowest priority: always be casting our 1-2-3 combo
-        QueueGCD(NextComboSingleTarget(), primaryTarget, GCDPriority.FlexibleCombo);
+        UseComboSingleTarget(primaryTarget);
+
+        Service.Log(string.Format("GCD: {0:N2}", GCD));
     }
 
     /*
-     * Determine the next action for our 1-2-3 combo
+     * Single-Target 1-2-3 Combo (Split Shot, Slug Shot, Clean Shot)
+     * Note: Covers heated variants automatically!
      */
-    private MCH.AID NextComboSingleTarget() => PrevCombo switch
+    private void UseComboSingleTarget(Actor? target)
     {
-        MCH.AID.SlugShot => MCH.AID.CleanShot,
-        MCH.AID.SplitShot => MCH.AID.SlugShot,
-        _ => MCH.AID.SplitShot
-    };
+        var ComboAction = PrevCombo switch
+        {
+            MCH.AID.SlugShot => MCH.AID.CleanShot,
+            MCH.AID.SplitShot => MCH.AID.SlugShot,
+            _ => MCH.AID.SplitShot
+        };
+
+        QueueGCD(ComboAction, target, GCDPriority.FlexibleCombo);
+    }
 
     /*
-     * Determine the next action for our tools
+     * Tools (Air Anchor, Drill, Chain Saw, Excavator)
+     * 1. Queue each with the following priorities:
+     *   a. Air Anchor
+     *   b. Drill 1
+     *   c. Chain Saw
+     *   d. Excavator (check for Excavator Ready buff as well)
+     *   e. Drill 2
      */
     private void UseSingleTargetTools(Actor? target)
     {
         QueueGCD(MCH.AID.AirAnchor, target, GCDPriority.AirAnchor);
 
+        // TODO: Make this work without reading the combat timer
+        // Opener usage
+        if (CombatTimer < 20) QueueGCD(MCH.AID.Drill, target, CD(MCH.AID.Drill) > 0 ? GCDPriority.OpenerSecondDrill : GCDPriority.Drill);
+
         // Change the priority depending on if we have 1 or 2 charges (determined by if Drill's CD > 0)
-        QueueGCD(MCH.AID.Drill, target, CD(MCH.AID.Drill) > 0 ? GCDPriority.SecondDrill : GCDPriority.Drill);
+        if (CombatTimer > 20) QueueGCD(MCH.AID.Drill, target, CD(MCH.AID.Drill) > 0 ? GCDPriority.SecondDrill : GCDPriority.Drill);
+
+        // Change the priority depending on if we have 1 or 2 charges (determined by if Drill's CD > 0)
+        // QueueGCD(MCH.AID.Drill, target, CD(MCH.AID.Drill) > 0 ? GCDPriority.SecondDrill : GCDPriority.Drill);
 
         QueueGCD(MCH.AID.ChainSaw, target, GCDPriority.ChainSaw);
 
@@ -165,33 +237,30 @@ public sealed class AimsucksMCH(RotationModuleManager manager, Actor player) : R
     }
 
     /*
-     * Always make sure we alternate between Double Check and Checkmate
-     * TODO: Change this to "use whichever has a lower cooldown" to make it more even
+     * Full Metal Field
+     * 1. Check that we have the Full Metal Machinist buff (from Barrel Stabilizer)
+     * 2. In 2-minute burst, use FMF right before Wildfire by looking at Wildfire's CD and comparing it to a GCD length
+     * 3. If our buff is about to expire, cast FMF in an emergency
      */
-    private void UseDoubleCheckOrCheckmate(Actor? target)
-    {
-        // Figure out what our next cast is going to be based on the previous "Check" ability
-        MCH.AID NextCheck = PrevCheck == MCH.AID.DoubleCheck ? MCH.AID.Checkmate : MCH.AID.DoubleCheck;
-
-        // Queue the next "Check" ability we want to cast
-        QueueOGCD(NextCheck, target, OGCDPriority.DoubleCheckOrCheckmate);
-
-        // Set "previous" ability to the one we just queued
-        PrevCheck = NextCheck;
-    }
-
     private void UseFullMetalField(Actor? target)
     {
         // We cannot use Full Metal Field with no Full Metal Machinist buff
         if (FullMetalMachinistLeft == 0) return;
 
-        // Condition 1: Always use Full Metal Field right before Wildfire
-        // Condition 2: In an emergency, use Full Metal Field before the buff expires
-        // Full Metal Field's priority is less than the rest of the tools, so it should happen after
-        if (WildfireCD <= GCDLength || FullMetalMachinistLeft <= 6)
+        if (CombatTimer < 20 && NextToolIn > GCDLength)
+            QueueGCD(MCH.AID.FullMetalField, target, GCDPriority.FullMetalField);
+
+        else if (WildfireCD <= GCDLength)
+            QueueGCD(MCH.AID.FullMetalField, target, GCDPriority.FullMetalField);
+
+        else if (FullMetalMachinistLeft <= 6)
             QueueGCD(MCH.AID.FullMetalField, target, GCDPriority.FullMetalField);
     }
 
+    /*
+     * Blazing Shot
+     * 1. Check that we're overheated
+     */
     private void UseBlazingShot(Actor? target)
     {
         // We cannot use Blazing Shot with no stacks of Overheated
@@ -199,6 +268,124 @@ public sealed class AimsucksMCH(RotationModuleManager manager, Actor player) : R
 
         QueueGCD(MCH.AID.BlazingShot, target, GCDPriority.BlazingShot);
     }
+
+    /*
+     * Double Check and Checkmate
+     * 1. Always make sure we alternate between Double Check and Checkmate depending on shortest CD
+     */
+    private void UseDoubleCheckOrCheckmate(Actor? target)
+    {
+        // Get cooldowns for both actions
+        var DoubleCheckIn = CD(MCH.AID.DoubleCheck);
+        var CheckmateIn = CD(MCH.AID.Checkmate);
+
+        // Compare the cooldowns of the two abilities and queue the one with a longer cooldown
+        MCH.AID NextCheck;
+        NextCheck = DoubleCheckIn > CheckmateIn ? MCH.AID.Checkmate : MCH.AID.DoubleCheck;
+
+        // Queue the next "Check" ability we want to cast
+        QueueOGCD(NextCheck, target, OGCDPriority.DoubleCheckOrCheckmate);
+    }
+
+    /*
+     * Reassemble
+     * 1. Do not use it inside a Hypercharge burst window
+     * 2. Always use when the next GCD is a tool
+     * 3. Always use ~5s before the fight starts
+     */
+    private void UseReassemble(Actor? target)
+    {
+        // If we are in a burst window or already have the buff, do not use it
+        if (OverheatedLeft > 0 || ReassembledLeft > 0) return;
+
+        // MCH.AID[] Tools = { MCH.AID.AirAnchor, MCH.AID.Drill, MCH.AID.ChainSaw, MCH.AID.Excavator };
+        // if(Tools.Contains(NextGCD)) QueueOGCD(MCH.AID.Reassemble, target, OGCDPriority.Reassemble);
+
+        // Check if a tool will be available within 1 GCD
+        if (NextToolIn <= GCDLength) QueueOGCD(MCH.AID.Reassemble, target, OGCDPriority.Reassemble);
+
+        // Use it before the countdown timer finishes, determined by Reassemble's EffectApplicationDelay below
+        if (World.Client.CountdownRemaining > 0) QueueOGCD(MCH.AID.Reassemble, target, OGCDPriority.Reassemble);
+    }
+
+    /*
+     * Barrel Stabilizer
+     * 1. Do not use it inside a Hypercharge burst window
+     * 2. Because this is a 2-minute cast, always use when Drill has a cooldown to ensure it's cast after Drill
+     *
+     * Note: This accounts for the very beginning of the fight, and since we will *always* have Drill on CD
+     * during the fight, it should just use Barrel Stabilizer every 2 minutes
+     *
+     * TODO: Figure out how to account for fight downtime if it becomes an issue
+     */
+    private void UseBarrelStabilizer(Actor? target)
+    {
+        // If we are in a burst window, do not use it
+        if (OverheatedLeft > 0) return;
+
+        // Use when Drill is on cooldown (so after the very first drill)
+        if (CD(MCH.AID.Drill) > 0) QueueOGCD(MCH.AID.BarrelStabilizer, target, OGCDPriority.BarrelStabilizer);
+    }
+
+    /*
+     * Wildfire
+     * 1. Do not use it inside a Hypercharge burst window
+     * 2. If in the opener, use it after all the tools have been used but before FMF has been used
+     * 3. If in a 2-minute burst window, use it right after Full Metal Machinist expires, which coincides with when
+     *     we use Full Metal Field
+     *
+     * TODO: Remove the requirement for the combat timer here
+     * TODO: Try to late weave Wildfire so it's the second oGCD
+     */
+    private void UseWildfire(Actor? target)
+    {
+        // If we are in a burst window, do not use it
+        // if (OverheatedLeft > 0) return;
+
+        // Opener usage
+        if (CombatTimer < 60)
+        {
+            // Use before FMF and after all the tools have been used
+            if (FullMetalMachinistLeft > 0 && NextToolIn > GCDLength)
+                QueueOGCD(MCH.AID.Wildfire, target, OGCDPriority.Wildfire);
+        }
+
+
+        // 2-minute burst window usage
+        if (CombatTimer > 60)
+        {
+            // Use after Full Metal Field and Hypercharge
+            if (FullMetalMachinistLeft == 0 && OverheatedLeft > 0)
+                QueueOGCD(MCH.AID.Wildfire, target, OGCDPriority.Wildfire);
+        }
+    }
+
+    /*
+     * Hypercharge
+     * 1. Do not use while Overheated
+     * 2. Do not use while heat is under 50 unless we have the Hypercharged buff
+     * 3. Do not use if a tool will cap within a Hypercharge window
+     * 4. Always use after FMF
+     */
+    private void UseHypercharge(Actor? target)
+    {
+        // Check multiple conditions to prevent action misuse
+        if (Heat < 50 && HyperchargedLeft == 0
+            || OverheatedLeft > 0
+            || ReassembledLeft > GCDLength) return;
+
+        // Use it after FMF has been used
+        // TODO: Try to see if we can late weave Hypercharge after FMF
+        if (FullMetalMachinistLeft > 0) return;
+
+        if (CappedToolIn > GCD + 7.5f)
+            QueueOGCD(MCH.AID.Hypercharge, target, OGCDPriority.Hypercharge);
+    }
+
+    /*
+     * Hypercharge
+     * 1. Make sure the next tool charge is in > 8 seconds before you queue Hypercharge
+     */
 
     /*
      * Determine the next action for our 1-2-3 combo
@@ -228,17 +415,22 @@ public sealed class AimsucksMCH(RotationModuleManager manager, Actor player) : R
     // }
 
     /*
-     * Provide values for how long it takes actions to actually "hit" the target
-     * Used primarily for pre-pull actions
+     * Effect Application Delay
+     * Note: This provides values for how long it takes actions to actually "hit" the target, which
+     * are used primarily for pre-pull actions
      */
     private float EffectApplicationDelay(MCH.AID aid) => aid switch
     {
+        MCH.AID.Reassemble => 5.0f,
         MCH.AID.AirAnchor => 0.50f,
         _ => 0
     };
 
     /*
-     * Helper function to queue GCD action
+     * Queue GCD helper function
+     * 1. Make sure priority is not set to "None"
+     * 2. Check the EffectApplicationDelay float to determine how long to cast it before pull
+     * 3. Compare action priorities to queue the correct GCD first
      */
     private void QueueGCD(MCH.AID aid, Actor? target, GCDPriority prio)
     {
@@ -258,7 +450,8 @@ public sealed class AimsucksMCH(RotationModuleManager manager, Actor player) : R
     }
 
     /*
-     * Helper function to queue oGCD action
+     * Queue oGCD helper function
+     * 1. Make sure priority is not set to "None"
      */
     private void QueueOGCD(MCH.AID aid, Actor? target, OGCDPriority prio, float basePrio = ActionQueue.Priority.Low)
     {
